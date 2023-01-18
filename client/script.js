e086aa628285d4fe81fccef8da9760fb08bdd366
IMATechnologist <imatechnologist30@gmail.com>
2023-01-18 17:09:10 +0530

    update server url

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handlesubmit = async (e) => {
   
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatai-ki5j.onrender.com', {
     method: "POST",
     headers: {
       'Content-Type': 'application/json'

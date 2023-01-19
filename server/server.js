8951447e0e71504eed8ee79e34410c2645236716
IMATechnologist <imatechnologist30@gmail.com>
2023-01-19 11:30:21 +0530

    Update to CSS

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.post('/', async (req, res) => {
             //model: "code-davinci-002",
             prompt: `${prompt}`,
             temperature: 0.5,
-            max_tokens: 3000,
+            max_tokens: 4000,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0,
